b0a0640c53f4b810fdfa454a6fe0624e9182cab7
Yicheng <yicheng.fzu@gmail.com>
2016-08-23 13:50:28 +0800

    add files

---
+++ shadowsocksR/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  shadowsocksR
+//
+//  Created by 称一称 on 16/8/23.
+//  Copyright © 2016年 yicheng. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
